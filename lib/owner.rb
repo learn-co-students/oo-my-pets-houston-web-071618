2f561ecaabf0c4c9209fb67c855719b095dcfb91
github username <github email address>
2018-07-24 12:59:08 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,79 @@
+require "pry"
+
 class Owner
-  # code goes here
+
+  @@owners = []
+
+  attr_reader :species
+  attr_accessor :name, :pets
+
+  def initialize(name)
+    @name = name
+    @@owners << self
+    @species = "human"
+    @pets = {:fishes=>[], :dogs=>[], :cats=>[]}
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.reset_all
+    @@owners = []
+  end
+
+  def self.count
+    @@owners.count
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    self.pets[:fishes] << new_fish
+  end
+
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    self.pets[:cats] << new_cat
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    self.pets[:dogs] << new_dog
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.each do |specie, species_array|
+      species_array.each do |species_object|
+        species_object.mood = "nervous"
+      end
+    end
+    @pets = []
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."  
+  end
+
 end
